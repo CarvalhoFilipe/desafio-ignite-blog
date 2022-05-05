f41be45ba78dede4edcdc228d974a8ca7012fba5
Filipe T. De Carvalho <fcarvalho@viceri.com.br>
2022-05-05 11:34:12 -0300

    update blog

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,7 +1,8 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import PrismicDom from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
-
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
@@ -26,20 +27,77 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+  return (
+    <>
+      <Head>title</Head>
+      <header className={styles.containerImage}>
+        <img src={post.data.banner.url} alt="image" />
+      </header>
+      <main className={commonStyles.container}>
+        {post.data.content.map(content => (
+          <div className={styles.posts}>
+            <h2>{content.heading}</h2>
+            {content.body.map(body => (
+              <div dangerouslySetInnerHTML={{__html:body.text}}/>
+            ))}
+          </div>
+        ))}
+      </main>
+    </>
+  );
+}
 
-//   // TODO
-// };
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient({});
+  const posts = await prismic.getByType<any>('post');
+  const paths = posts.results.map(post => {
+    return {
+      params: {
+        id: post.uid,
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+};
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
-
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByUID('posts', String(slug));
+  const results = response.data.content.map(post => {
+  return {
+      heading: post.heading,
+      body: post.body.map(body => body),
+    };
+  });
+ 
+  return {
+    props: {
+      post: {
+        first_publication_date: response.first_publication_date,
+        data: {
+          title: PrismicDom.RichText.asText(response.data.title),
+          banner: {
+            url: response.data.banner.url,
+          },
+          author: response.data.author,
+          content: [...results].map(content => ({
+            heading: content.heading,
+            body: [{
+              text: PrismicDom.RichText.asHtml(content.body),
+            }],
+          })),
+        },
+      },
+    },
+  };
+};
